ae99d5b694d58fad14a8e3df240efdad4a8d2601
N1 <pathaknishant003@gmail.com>
2023-12-01 12:03:21 +0530

    add report

---
+++ sample.py
@@ -0,0 +1,24 @@
+Why Does Data Need Processing through Data Engineering?
+Data engineers play a crucial role in designing, operating, and supporting the increasingly complex environments that power modern data analytics. Historically, data engineers have carefully crafted data warehouse schemas, with table structures and indexes designed to process queries quickly to ensure adequate performance. With the rise of data lakes, data engineers have more data to manage and deliver to downstream data consumers for analytics. Data that is stored in data lakes may be unstructured and unformatted – it needs attention from data engineers before the business can derive value from it.
+
+Fortunately, once a data set has been fully cleaned and formatted through data engineering, it’s easier and faster to read and understand. Since businesses are creating data constantly, it’s important to find software that will automate some of these processes.
+
+The right software stack will extract a huge amount of information and value from your data, which creates end-to-end journeys for the data known as “data pipelines.” As the information travels through the pipeline, it may be transformed, enriched and summarized several times.
+    
+    Data Engineering Tools and Skills
+Data engineers use many different tools to work with data. They use a specialized skill set to create end-to-end data pipelines that move data from source systems to target destinations.
+
+Data engineers work with a variety of tools and technologies, including:
+
+ETL Tools: ETL (extract, transform, load) tools move data between systems. They access data, then apply rules to “transform” the data through steps that make it more suitable for analysis.
+SQL: Structured Query Language (SQL) is the standard language for querying relational databases.
+Python: Python is a general programming language. Data engineers may choose to use Python for ETL tasks.
+Cloud Data Storage: Including Amazon S3, Azure Data Lake Storage (ADLS), Google Cloud Storage, etc.
+Query Engines: Engines run queries against data to return answers. Data engineers may work with engines like Dremio Sonar, Spark, Flink, and others.
+Data Engineering vs. Data Science
+Data engineering and data science are two complementary skills. Data engineers help make data reliable and consistent for analysis. Data scientists need reliable data for machine learning, data exploration, and other analytical projects involving large data sets. Data scientists may rely on data engineers to find and prepare data for their analysis.
+
+Data Engineering with Dremio
+Dremio simplifies data management for data engineers and a single, unified access point for all enterprise data for BI and ad-hoc self-service. Learn more about the data lakehouse with Dremio.
+
+Ready to go deeper? Read a more technical article on data engineering.
