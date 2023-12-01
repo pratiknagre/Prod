bafe5a686f9e7c4cea440b5d46573fe498815560
N1 <pathaknishant003@gmail.com>
2023-12-01 12:00:52 +0530

    add

---
+++ new.py
@@ -0,0 +1,9 @@
+What Do Data Engineers Do?
+Data engineering is a skill that is in increasing demand. Data engineers are the people who design the system that unifies data and can help you navigate it. Data engineers perform many different tasks including:
+
+Acquisition: Finding all the different data sets around the business
+Cleansing: Finding and cleaning any errors in the data
+Conversion: Giving all the data a common format
+Disambiguation: Interpreting data that could be interpreted in multiple ways
+Deduplication: Removing duplicate copies of data
+Once this is done, data may be stored in a central repository such as a data lake or data lakehouse. Data engineers may also copy and move subsets of data into a data warehouse.
